922f60d6b2f914df987c9614b71009beed9d8ce4
Oleksandra Pinchuk <ppinchukk.25@gmail.com>
2026-04-05 19:00:57 -0500

    solve tasks

---
+++ js/task-4.js
@@ -0,0 +1,25 @@
+const registerForm = document.querySelector(".login-form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  };
+
+  const result = {
+    email: email,
+    password: password
+  };
+  
+  console.log(result);
+
+  form.reset();
+}
+
---
+++ js/task-5.js
@@ -0,0 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const button = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", handleButton);
+
+function handleButton(event) {
+  const finalColor = getRandomHexColor();
+
+  colorSpan.textContent = finalColor;
+
+  body.style.backgroundColor = finalColor;
+}
